4b29d7a09dad9fd6ebdf2da958ea5233f7b284f4
Rike <hello@ueberdiespree.de>
2019-06-21 23:44:18 +0200

    lambda send ses mail function

--- R1D079-serveless-lambda/handler.js
+++ R1D079-serveless-lambda/handler.js
@@ -1,14 +1,42 @@
 'use strict';
 
-module.exports.newuser = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+var aws = require("aws-sdk");
+var ses = new aws.SES({
+  region: "eu-west-1"
+});
+
+exports.newuser = function(event, context) {
+  console.log("Incoming: ", event);
+  // var output = querystring.parse(event);
+
+  var eParams = {
+    Destination: {
+      ToAddresses: ["ueberdiespree@gmail.com"]
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "bdlfdsf"
+        }
+      },
+      Subject: {
+        Data: "Ses Test Email"
+      }
+    },
+    Source: "ueberdiespree@gmail.com"
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  console.log("===SENDING EMAIL===");
+  var email = ses.sendEmail(eParams, function(err, data) {
+    if (err) {
+      console.log(err);
+      context.fail(err);
+    } else {
+      console.log("===EMAIL SENT===");
+      console.log("EMAIL CODE END");
+      console.log("EMAIL: ", email);
+      console.log(data);
+      context.succeed(event);
+    }
+  });
 };
